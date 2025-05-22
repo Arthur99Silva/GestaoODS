49a2d3be56d37e702541d5acf93e39aed28beaf9
gustavo-coro <gustavohenrique13579@gmail.com>
2025-05-22 17:07:20 -0300

    add paginação

--- gestao-front/src/app/customers/customer-list/customer-list.component.ts
+++ gestao-front/src/app/customers/customer-list/customer-list.component.ts
@@ -1,5 +1,5 @@
 // src/app/customers/customer-list/customer-list.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import {
@@ -9,31 +9,65 @@ import {
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { ApiService } from '../../services/api.service';
+
+function getPortuguesePaginatorIntl() {
+  const paginatorIntl = new MatPaginatorIntl();
+  paginatorIntl.itemsPerPageLabel = 'Itens por página:';
+  paginatorIntl.nextPageLabel = 'Próxima página';
+  paginatorIntl.previousPageLabel = 'Página anterior';
+  paginatorIntl.firstPageLabel = 'Primeira página';
+  paginatorIntl.lastPageLabel = 'Última página';
+  paginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+    return `${page * pageSize + 1} - ${Math.min((page + 1) * pageSize, length)} de ${length}`;
+  };
+  return paginatorIntl;
+}
 
 @Component({
   standalone: true,
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.css'],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: getPortuguesePaginatorIntl() }
+  ],
   imports: [
     CommonModule,
     MatCardModule,
     MatTableModule,
     MatIconModule,
     MatButtonModule,
-    RouterLink            // para uso de routerLink nos botões
+    RouterLink,
+    MatPaginatorModule
   ]
 })
 export class CustomerListComponent implements OnInit {
   dataSource = new MatTableDataSource<any>();
   columns = ['nome', 'email', 'cpf_cnpj', 'telefone', 'endereco', 'actions'];
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private api: ApiService) {}
 
   ngOnInit() {
-    this.api.getCustomers().subscribe(list => {
-      this.dataSource.data = list;
+    this.loadCustomers();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  loadCustomers() {
+    this.api.getCustomers().subscribe({
+      next: (list) => {
+        this.dataSource.data = list;
+        // Set pagination options
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => console.error('Erro ao carregar os clientes', err)
     });
   }
 }
--- gestao-front/src/app/sales/sales-list/sales-list.component.ts
+++ gestao-front/src/app/sales/sales-list/sales-list.component.ts
@@ -1,23 +1,37 @@
 // src/app/companies/sales-list/sales-list.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import {
-  MatTableModule,
-  MatTableDataSource
-} from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { ApiService } from '../../services/api.service';
-
 import { DateFormatPipe } from '../../pipes/date-format.pipe';
+
+function getPortuguesePaginatorIntl() {
+  const paginatorIntl = new MatPaginatorIntl();
+  paginatorIntl.itemsPerPageLabel = 'Itens por página:';
+  paginatorIntl.nextPageLabel = 'Próxima página';
+  paginatorIntl.previousPageLabel = 'Página anterior';
+  paginatorIntl.firstPageLabel = 'Primeira página';
+  paginatorIntl.lastPageLabel = 'Última página';
+  paginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+    return `${page * pageSize + 1} - ${Math.min((page + 1) * pageSize, length)} de ${length}`;
+  };
+  return paginatorIntl;
+}
 
 @Component({
   standalone: true,
   selector: 'app-sales-list',
   templateUrl: './sales-list.component.html',
   styleUrls: ['./sales-list.component.css'],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: getPortuguesePaginatorIntl() }
+  ],
   imports: [
     CommonModule,
     MatCardModule,
@@ -25,19 +39,35 @@ import { DateFormatPipe } from '../../pipes/date-format.pipe';
     MatButtonModule,
     MatIconModule,
     RouterLink,
-    DateFormatPipe // Add the pipe here
+    DateFormatPipe,
+    MatPaginatorModule
   ]
 })
 export class SalesListComponent implements OnInit {
   dataSource = new MatTableDataSource<any>();
-  columns = ['valor_total', 'data_venda', 'nota_fiscal', 'fk_cpf_cnpj_cliente', 'nome_forma_pagamento', 'fk_cpf_funcionario', 'actions'];
+  columns = ['valor_total', 'data_venda', 'nota_fiscal', 'fk_cpf_cnpj_cliente', 
+             'nome_forma_pagamento', 'fk_cpf_funcionario', 'actions'];
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private api: ApiService) {}
 
   ngOnInit() {
-    this.api.getSales().subscribe(list => {
-      console.log(list);
-      this.dataSource.data = list;
+    this.loadSales();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  loadSales() {
+    this.api.getSales().subscribe({
+      next: (list) => {
+        this.dataSource.data = list;
+        // Set pagination options
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => console.error('Erro ao carregar as vendas', err)
     });
   }
 }
