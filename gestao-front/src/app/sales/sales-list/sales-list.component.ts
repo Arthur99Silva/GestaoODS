02216dbe08a2320bb0362002aa478a97011080e6
Arthur99Silva <arthurassilva99@gmail.com>
2025-05-26 21:17:47 -0300

    CPF funcionario busca vendas

--- gestao-front/src/app/sales/sales-list/sales-list.component.ts
+++ gestao-front/src/app/sales/sales-list/sales-list.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import {
   MatTableModule,
   MatTableDataSource
@@ -10,6 +11,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   standalone: true,
@@ -22,19 +26,33 @@ import { ApiService } from '../../services/api.service';
     MatTableModule,
     MatButtonModule,
     MatIconModule,
-    RouterLink
-  ]
+    RouterLink,
+    FormsModule,MatFormFieldModule, // <-- Adicione esta linha
+    MatInputModule, // <-- Adicione esta linha
+    FormsModule // <-- Certifique-se que está presente
+  ],
+  providers: [CurrencyPipe, DatePipe]
 })
 export class SalesListComponent implements OnInit {
   dataSource = new MatTableDataSource<any>();
+  originalData: any[] = []; // Armazena a lista completa
+  cpfFilter: string = ''; // Variável para o filtro
   columns = ['valor_total', 'data_venda', 'actions'];
 
   constructor(private api: ApiService) {}
 
   ngOnInit() {
     this.api.getSales().subscribe(list => {
-      console.log(list);
-      this.dataSource.data = list;
+      this.originalData = list; // Salva os dados originais
+      this.dataSource.data = this.originalData;
     });
+  }
+
+  // Método para aplicar o filtro
+  applyFilter() {
+    const filterValue = this.cpfFilter.trim().toLowerCase();
+    this.dataSource.data = this.originalData.filter(sale => 
+      sale.fk_cpf_funcionario.toLowerCase().includes(filterValue)
+    );
   }
 }
