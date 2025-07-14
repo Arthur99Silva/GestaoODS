b47408c34409bb34bada7c3151c78e76164f6bc6
Arthur99Silva <arthurassilva99@gmail.com>
2025-07-14 17:14:22 -0300

    Merge feito

--- gestao-back/src/database.config.ts
+++ gestao-back/src/database.config.ts
@@ -1,20 +1,37 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import 'dotenv/config'; // Importante para carregar as variáveis
+import { Cliente } from "./Cliente/entite/cliente.entity";
+import { FormaPagamento } from "./Forma_Pagamento/entite/forma-pagamento.entity";
+import { Funcionario } from "./Funcionario/entite/funcionario.entity";
+import { ItemProduto } from "./Item_Produto/entite/item-produtos.entity";
+import { Pedido } from "./Pedido/entite/pedido.entity";
+import { Empresa } from "./Empresa/entity/empresa.entity";
+import { Fornecedor } from "./Fornecedor/entity/fornecedor.entity";
+import { Produto } from "./Produto/entity/produto.entity";
+import { DataSource } from 'typeorm';
+import { AuthData } from "./Auth/entity/auth-data.entity";
 
-export const DatabaseConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST as string, // <-- CORREÇÃO AQUI
-  port: parseInt(process.env.DB_PORT as string, 10), // <-- CORREÇÃO AQUI
-  username: process.env.DB_USERNAME as string, // <-- CORREÇÃO AQUI
-  password: process.env.DB_PASSWORD as string, // <-- CORREÇÃO AQUI
-  database: process.env.DB_DATABASE as string, // <-- CORREÇÃO AQUI
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
-  ssl: process.env.DB_HOST !== 'localhost',
-  extra: {
-    ssl:
-      process.env.DB_HOST !== 'localhost'
-        ? { rejectUnauthorized: false }
-        : null,
-  },
-};
+const dataSourceOptions = new DataSource({
+    type: 'postgres',
+    host: 'localhost', // Endereço do servidor do banco de dados --
+    port: 5432, // Porta padrão do PostgreSQL
+    username: 'postgres',    // Nome do usuário do banco de dados --
+    password: '1234',   // Senha do banco de dados --
+    database: 'ods', // Nome do banco de dadosw
+    entities: [
+        Cliente,
+        FormaPagamento,
+        Funcionario,
+        ItemProduto,
+        Pedido,
+        Empresa,
+        Fornecedor,
+        Produto,
+        AuthData,
+    ], // Entidades
+    synchronize: true, // Sincroniza o esquema do banco de dados, apenas para desenvolvimento
+    logging: true, //Ativa logs para visualizarmos possíveis erros,
+    migrations: [
+        'src/migrations/*.ts', // Caminho para as migrações
+    ],
+})
+
+export default dataSourceOptions;
