9c09f77892f8a423a9263dddf2a8b848761ee796
Bruno Mamede <brunomamede2003trabalho@gmail.com>
2025-07-14 19:04:26 -0300

    Última caçada de bugs feita manualmente. Consertada a procura por empresas e retirada de seed antigo não criptografado que era realizado no banco de dados para testar o login.

--- gestao-back/src/main.ts
+++ gestao-back/src/main.ts
@@ -1,9 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Repository } from 'typeorm';
-import { AuthData } from './Auth/entity/auth-data.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,28 +11,16 @@ async function bootstrap() {
     allowedHeaders: '*',
     Credentials: true,
   });
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true,               // Permite transformar payloads em instâncias de classes
-      whitelist: true,               // Remove propriedades que não estão no DTO
-      forbidNonWhitelisted: true     // Retorna erro se propriedades extras forem passadas
+      transform: true, // Permite transformar payloads em instâncias de classes
+      whitelist: true, // Remove propriedades que não estão no DTO
+      forbidNonWhitelisted: true, // Retorna erro se propriedades extras forem passadas
     }),
   );
 
-  //Código de teste de usuário
-  const repo = app.get<Repository<AuthData>>(getRepositoryToken(AuthData));
-
-  // Checa se o usuário de teste já existe antes de criar
-  const exists = await repo.findOneBy({ email: 'teste@exemplo.com' });
-  if (!exists) {
-    await repo.save({ email: 'teste@exemplo.com', senha: '1234' });
-    console.log('Usuário de teste criado com sucesso!');
-  } else {
-    console.log('Usuário de teste já existe.');
-  }
-  //Fim de código de teste de usuário
-
   await app.listen(3000);
 }
+
 bootstrap();
